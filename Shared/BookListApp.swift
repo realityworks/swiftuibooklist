7b8abdad4c91a4af15adafd22a27a8d2f183b1cc
Piotr Suwara <psuwara@realityworks.com.au>
2020-08-31 19:28:12 +0300

    Initial project

---
+++ Shared/BookListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookListApp.swift
+//  Shared
+//
+//  Created by Piotr Suwara on 31/8/20.
+//
+
+import SwiftUI
+
+@main
+struct BookListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
